49e515072f8e2c00d33388beb3604fc3b8361fa5
SytxNox <seven_night_w@hotmail.com>
2025-03-14 03:09:48 +0000

    feat: implement target's color change when it get hited.

--- Assets/Scripts/Object/TargetForBuildingRise.cs
+++ Assets/Scripts/Object/TargetForBuildingRise.cs
@@ -5,12 +5,13 @@ public class TargetForBuildingRise : MonoBehaviour
     public bool isHit = false;
     public BuildingGenerater buildingGenerater;
     public int hitFlag;
-
+    private MeshRenderer meshRenderer;
     //public GameObject buildingGroup;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         hitFlag = 0;
+        meshRenderer = GetComponent<MeshRenderer>();
     }
 
     // Update is called once per frame
@@ -20,9 +21,11 @@ public class TargetForBuildingRise : MonoBehaviour
         if (isHit) {
             if(hitFlag % 2 == 1){
                 buildingGenerater.StartingRising();
+                meshRenderer.materials[0].color = Color.red;
             }
         else{
                 buildingGenerater.destoryBuilding();
+                meshRenderer.materials[0].color = Color.green;
             }
             isHit = false;
         }
